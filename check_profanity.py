f6e5f5665d3e6f18cdb628ee0f98739b304bbf04
GHZ <kuhzcn@gmail.com>
2017-10-05 11:54:45 +0800

    finish profanity check function

--- check_profanity.py
+++ check_profanity.py
@@ -1,6 +1,22 @@
+import urllib
 def read_text():
      quotes = open("/Users/gu/Desktop/Python/check_profanity/movie_quotes.txt")
      contents_of_file = quotes.read()
-     print(contents_of_file)
+     #print(contents_of_file)
+     profanity_check(contents_of_file)
      quotes.close()
+
+def profanity_check(text):
+    connection = urllib.urlopen("http://www.wdylike.appspot.com/?q="+text)
+    output = connection.read()
+    #print(output)
+    connection.close()
+    if "false" in output:
+        print("it's good")
+    elif "ture" in output:
+        print("watch your language")
+    else:
+        print("can't scan the text")
+
+
 read_text()
